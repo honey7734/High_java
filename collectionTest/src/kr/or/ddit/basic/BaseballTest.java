53355af370cbf6d66cc30881ec2baa05cf92772b
honey7734 <gusdml7243@gmail.com>
2022-02-25 09:48:51 +0900

    Baseball게임 선생님 버전

--- collectionTest/src/kr/or/ddit/basic/BaseballTest.java
+++ collectionTest/src/kr/or/ddit/basic/BaseballTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /*
  	문제) Set을 활용하여 숫자 야구 게임 프로그램을 작성하시오.
@@ -29,49 +30,108 @@ import java.util.Scanner;
     당신은 4번째만에 맞췄습니다.
  */
 public class BaseballTest {
+	private ArrayList<Integer> numList = new ArrayList<Integer>(); // 난수가 저장될 List
+	private ArrayList<Integer> userList = new ArrayList<Integer>(); // 사용자가 입력한 값이 저장될 List
+
+	private int strike;  // 스트라이크 개수
+	private int ball;  // 볼 개수
+	private Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		HashSet<Integer> num = new HashSet<Integer>();
-		while (num.size() < 3) {
-			int rnd = (int)(Math.random() * (9-1+1) + 1);
-			num.add(rnd);
+//		BaseballTest b = new BaseballTest();
+//		b.gameStart();
+		
+		new BaseballTest().gameStart();
+	}
+	
+	// 게임이 시작되는 메서드
+	public void gameStart() {
+		
+		// 난수 만드는 메서드 호출
+		getNum();
+		
+		// 확인용
+//		System.out.println("컴퓨터 난수 : " + numList);
+		
+		int cnt = 0;   // 몇번째만에 맞췄는지를 저장하는 변수
+		do{
+			cnt++;
+			
+			// 사용자로부터 입력받는 메서드 호출
+			inputNum();
+			
+			// 볼 카운트 구하기
+			ballCount();
+	
+		}while(strike != 3);
+	
+		System.out.println();
+		System.out.println("축하합니다.");
+		System.out.println("당신은 " + cnt + "번째만에 맞췄습니다.");
+	}
+	
+	
+	// 1~9사이의 서로 다른 난수 3개를 만들어서 List에 저장하는 메서드
+	// (Set이용)
+	private void getNum() {
+		Set<Integer> numSet = new HashSet<Integer>();
+		
+		// 1~9사시의 난수 만들기
+		while (numSet.size() < 3) {
+			numSet.add((int)(Math.random() * 9 + 1));
 		}
-
-		ArrayList<Integer> numList = new ArrayList<Integer>(num);
 		
+		// 만들어진 난수를 List에 저장한다.
+		numList = new ArrayList<Integer>(numSet);
+		
+		// List의 메서드를 섞어준다.
 		Collections.shuffle(numList);
-		System.out.println(numList);
-		
-		int s;
-		int b;
-		int count = 0;
-		
-		do {
-			s = 0;
-			b = 0;
-			count++;
-			
-			System.out.print("숫자 입력 > ");
-			int i1 = Integer.parseInt(sc.next());
-			int i2 = Integer.parseInt(sc.next());
-			int i3 = Integer.parseInt(sc.next());
-			
-			if(i1 == numList.get(0)) s++;
-			if(i2 == numList.get(1)) s++;
-			if(i3 == numList.get(2)) s++;
-			
-			if(i1 == numList.get(1)||i1 == numList.get(2)) b++;
-			if(i2 == numList.get(0)||i1 == numList.get(2)) b++;
-			if(i3 == numList.get(0)||i3 == numList.get(1)) b++;
-			
-			
-			System.out.println(i1 + " " + i2 + " " + i3 + " => " + s + "S " + b + "B");
-		}while(s != 3);
-		
-		System.out.println("축하합니다...");
-		System.out.println("당신은 " + count +"번째만에 맞췄습니다");
 		
 	}
-
+	
+	// 사용자로부터 3개의 정수를 입력받아서 List에 저장하는 메서드
+	// 입력한 정수는 중복되지 않게 한다.
+	private void inputNum() {
+		int n1, n2, n3;  // 입력한 정수가 저장될 변수 선언
+		
+		do {
+			System.out.print("숫자입력 : ");
+			n1 = scan.nextInt();
+			n2 = scan.nextInt();
+			n3 = scan.nextInt();
+			
+			if(n1 == n2 || n1 == n3 || n2 == n3) {
+				System.out.println("중복되는 숫자는 입력할 수 없습니다.");
+				System.out.println("다시 입력하세요.");
+			}
+		}while(n1 == n2 || n1 == n3 || n2 == n3);
+		
+		userList = new ArrayList<Integer>();
+		userList.add(n1);
+		userList.add(n2);
+		userList.add(n3);
+		
+	}
+	
+	// 스트라이크 볼을 판정하고 결과를 출력하는 메서드
+	private void ballCount() {
+		strike = 0;
+		ball = 0;
+		
+		for(int i = 0; i < userList.size(); i++) {
+			for(int j = 0; j < numList.size(); j++) {
+				if(userList.get(i) == numList.get(j)) {
+					if(i == j) {
+						strike++;
+					}else {
+						ball++;
+					}
+				}
+			}
+		}
+		
+		// 볼카운트 결과 출력하기
+		System.out.printf("%d %d %d => %dS %dB\n", userList.get(0), userList.get(1), userList.get(2), strike, ball);
+	}
 }
+
