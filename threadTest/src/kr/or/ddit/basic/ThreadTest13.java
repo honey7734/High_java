5346291efd61d520f2ce10cbbe712a5d9d5e2ae7
honey7734 <gusdml7243@gmail.com>
2022-03-07 09:43:56 +0900

    과제 - 경마프로그램 선생님코드

--- threadTest/src/kr/or/ddit/basic/ThreadTest13.java
+++ threadTest/src/kr/or/ddit/basic/ThreadTest13.java
@@ -1,7 +1,6 @@
 package kr.or.ddit.basic;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 /*
  	10마리의 말들이 경주하는 경마 프로그램 작성하기
@@ -25,118 +24,181 @@ import java.util.Collections;
  	
  */
 public class ThreadTest13 {
-	public static ArrayList<String> finish = new ArrayList<String>();
-	public static ArrayList<Horse> horses = new ArrayList<Horse>();
 	
 	public static void main(String[] args) {
-		PrintGame pg = new PrintGame();
-		pg.setDaemon(true);
+		Horse[] horses = new Horse[] {
+			new Horse("01번말"),	
+			new Horse("02번말"),	
+			new Horse("03번말"),	
+			new Horse("04번말"),	
+			new Horse("05번말"),	
+			new Horse("06번말"),	
+			new Horse("07번말"),	
+			new Horse("08번말"),	
+			new Horse("09번말"),	
+			new Horse("10번말")	
+		};
 		
-		for(int i = 1; i <= 10; i++) {
-			horses.add(new Horse(i+"번말"));
+		GameState gs = new GameState(horses);
+		
+		// 경주를 시작한다.
+		for(Horse h : horses) {
+			h.start();
 		}
-		pg.start();
 		
-		for(Horse horse : horses) {
-			horse.start();
-		}
+		gs.start();		// 말들의 현재위치를 나타내는 쓰레드도 시작한다
 		
-		
-		for(Horse horse : horses) {
+		// 모든 말들의 경주가 끝날 때까지 기다린다.
+		for(Horse h : horses) {
 			try {
-				horse.join();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-			}
+				h.join();
+			} catch (InterruptedException e) {}
+		}
+	
+		try {
+			gs.join();
+		} catch (InterruptedException e) {}
+		
+		System.out.println();
+		System.out.println("경기 끝...");
+		System.out.println();
+		
+		// 등수의 오름차순으로 정렬하기
+		Arrays.sort(horses);    // 배열을 정렬하기
+		for(Horse h : horses) {
+			System.out.println(h);
 		}
 		
-		Collections.sort(horses);
-		System.out.println();
-		System.out.println("경기결과");
-		for(int i=0; i<horses.size(); i++) {
-			System.out.println((i+1) + "등 : " + horses.get(i).gettingName());
-		}
-
 	}
-
 }
 
 class Horse extends Thread implements Comparable<Horse>{
-	private String name;
-	private int rank;
-	private int num; //현재위치
-	
-	
-	
-	public Horse(String name) {
-		this.name = name;
-		this.rank = 1;
-		this.num = 1;
-	}
-	
+	public static int currentRank = 0;  // 말들의 등수를 결정할 변수
 
-	@Override
-	public void run() {
-		for(num = 1; num <= 50; num++) {
-			try {
-				num++;
-				Thread.sleep((int)(Math.random() * 300 + 201));
-			} catch (InterruptedException e) {
-
-			}
-			
-		}
-		ThreadTest13.finish.add(name);
-		rank = ThreadTest13.finish.size();
-	}
+	private String horseName;    // 말이름
+	private int rank;	    // 등수
+	private int position;   // 현재위치
 	
-	public String gettingName() {
-		return name;
+	
+	
+	public Horse(String horseName) {
+		this.horseName = horseName;
 	}
+
+	public String getHorseName() {
+		return horseName;
+	}
+
+
+	public void setHorseName(String horseName) {
+		this.horseName = horseName;
+	}
+
 
 	public int getRank() {
 		return rank;
 	}
 
-	public int getNum() {
-		return num;
+
+	public void setRank(int rank) {
+		this.rank = rank;
 	}
 
 
-	@Override
-	public int compareTo(Horse horse) {
-		
-		return ((Integer)this.rank).compareTo(horse.getRank());
+	public int getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(int position) {
+		this.position = position;
 	}
 
 	
+	@Override
+	public String toString() {
+		return "경주마 " + horseName + "은(는)" + rank + "등 입니다.";
+	}
+
+	
+	// 등수의 오름차순 정렬 기준 설정하기
+	@Override
+	public int compareTo(Horse horse) {
+		return Integer.compare(rank,horse.getRank());
+	}
+
+	@Override
+	public void run() {
+		// 경주를 진행하는 쓰레드 처리
+		
+		// 1~50 구간 진행
+		for(int i=1; i<=50; i++) {
+			this.position = i;  // 현재 구간을 말의 현재 위치로 한다.
+			
+			try {
+				Thread.sleep((int)(Math.random() * 500));
+			} catch (InterruptedException e) {
+				
+			}
+		}
+		
+		// 한 마리의 말이 경주가 끝난 후에 등수를 구한다.
+		currentRank++;
+		this.rank = currentRank;
+	}
 	
 	
 }
 
-class PrintGame extends Thread{
+// 경기 중에 말의 현재 위치를 나타내는 쓰레드
+/*
+	예)
+	01번말 : --->----------------------------------------------
+	02번말 : ------------>-------------------------------------
+	...
+	10번말 : --->----------------------------------------------
+	
+*/
+
+class GameState extends Thread{
+	private Horse[] horses;
+	
+	// 생성자 - 경주에 참여한 말들이 저장된 배열을 받아서 초기화한다.
+	public GameState (Horse[] horses) {
+		this.horses = horses;
+	}
+	
 	@Override
 	public void run() {
-		while(true) {
-			
-		for(int i = 0; i < ThreadTest13.horses.size(); i++) {
-			System.out.print(ThreadTest13.horses.get(i).gettingName() + " : ");
-			
-			for(int j = 1; j <= 50; j++) {
-				if(j==ThreadTest13.horses.get(i).getNum()) {
-					System.out.print(">");
-				}else {
-					System.out.print("-");
-				}
-				
+		while (true) {
+			// 모든 말들의 경주가 종료되면 반복문을 탈출한다.
+			if(Horse.currentRank==horses.length) {
+				break;
 			}
-			System.out.println();
-		}
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-		System.out.println("-------------------------------------------------------------");
+			for(int i=1; i<=10; i++) {
+				System.out.println();
+			}
+			
+			// 01번말 : --->---------------------------------------------- 
+			for(int i=0; i<horses.length; i++) {
+				System.out.print(horses[i].getHorseName() + " : ");
+				for(int j=1; j<=50; j++) {
+					// 말의 현재위치와 구간값이 같은지 여부 검사
+					
+					if(horses[i].getPosition() == j) {
+						System.out.print(">");
+					}else {
+						System.out.print("-");
+					}
+				}
+				System.out.println();  // 줄바꿈
+			}
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO: handle exception
+			}
 		}
 	}
 }
