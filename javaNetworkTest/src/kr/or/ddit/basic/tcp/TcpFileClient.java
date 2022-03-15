2ae0db957dc440c0e3770dee072abf07781979ea
honey7734 <gusdml7243@gmail.com>
2022-03-15 11:42:05 +0900

    파일전송 시스템 선생님코드

--- javaNetworkTest/src/kr/or/ddit/basic/tcp/TcpFileClient.java
+++ javaNetworkTest/src/kr/or/ddit/basic/tcp/TcpFileClient.java
@@ -1,33 +1,60 @@
 package kr.or.ddit.basic.tcp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+/*
+클라이언트는 d드라이브의 d_other폴더에 있는 '펭귄.jpg'파일을 서버로 전송 
+ */
 import java.net.Socket;
 
 public class TcpFileClient {
-
-	public static void main(String[] args) throws IOException{
-		Socket socket = new Socket("localhost",7777);
-		System.out.println("서버에 연결되었습니다...");
-		System.out.println();
-		
+	private Socket socket;
+	private BufferedInputStream  bis;
+	private BufferedOutputStream bos;
+	
+	public void ClientStart(){
 		File file = new File("d://d_other/펭귄.jpg");
 		
-		OutputStream out = socket.getOutputStream();
-        FileInputStream fin = new FileInputStream(file);
-        System.out.println("파일전송시작...");
-        
-        int data = 0;
-        while ((data = fin.read())!= -1) {
-			out.write(data);
+		if(!file.exists()) {
+			System.out.println(file.getName() + "파일이 없습니다");
+			System.out.println("파일 전송 작업을 중단합니다");
+			return;
 		}
 		
-        System.out.println("파일전송완료...");
-        fin.close();
-        out.close();
+		try {
+			socket = new Socket("localhost",7777);
+			System.out.println("파일 전송 시작...");
+			
+			bis = new BufferedInputStream(new FileInputStream(file));
+			bos = new BufferedOutputStream(socket.getOutputStream());
+			
+			byte[] temp = new byte[1024];
+			int len = 0;
+			
+			// 파일 내용을 읽어와 소켓으로 출력한다.
+			while((len = bis.read()) > 0) {
+				bos.write(temp, 0, len);
+			}
+			bos.flush();
+			System.out.println("파일 전송 완료...");
+			
+		} catch (Exception e) {
+			System.out.println("파일 전송 실패!!!");
+			e.printStackTrace();
+		}finally {
+			if(bos != null) try{bos.close();}catch(IOException e) {}
+			if(bis != null) try{bis.close();}catch(IOException e) {}
+			if(socket != null) try{socket.close();}catch(IOException e) {}
+		}
 		
+	}
+	
+	public static void main(String[] args){
+		
+		new TcpFileClient().ClientStart();
 		
 	}
 
