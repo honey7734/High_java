c4a7a8d28b110b576a363ab98929a9ab3627a26b
honey7734 <gusdml7243@gmail.com>
2022-03-21 11:19:24 +0900

    폰북과제 추가기능 선생님코드 참고

--- jdbcTest/src/kr/or/ddit/basic/JdbcTest06.java
+++ jdbcTest/src/kr/or/ddit/basic/JdbcTest06.java
@@ -33,39 +33,45 @@ import kr.or.ddit.util.DBUtill;
 */
 
 public class JdbcTest06 {
-	Scanner scan = new Scanner(System.in);
-	Connection conn = null;
-	Statement stmt = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
+	private Scanner scan = new Scanner(System.in);
+	
+	private Connection conn;
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 	
 	public static void main(String[] args) {
 		new JdbcTest06().start();
 	}
+	
+	private int displayMenu() {
+		System.out.println("---------------------------");
+		System.out.println("	== 작업 선택 ==");
+		System.out.println("  1. 자료 추가");
+		System.out.println("  2. 전체 항목 수정");
+		System.out.println("  3. 자료 삭제");
+		System.out.println("  4. 전체 자료 출력");
+		System.out.println("  5. 수정");
+		System.out.println("  0. 작업 끝");
+		System.out.println("---------------------------");
+		return scan.nextInt();
+	}
 
 	private void start() {
 		while(true) {
-			System.out.println("---------------------------");
-			System.out.println("	== 작업 선택 ==");
-			System.out.println("  1. 자료 추가");
-			System.out.println("  2. 자료 수정");
-			System.out.println("  3. 자료 삭제");
-			System.out.println("  4. 전체 자료 출력");
-			System.out.println("  0. 작업 끝");
-			System.out.println("---------------------------");
-			int input = scan.nextInt();
-			
+			int input = displayMenu();
 			switch (input) {
-			case 1: insert(); break;
-			case 2: update(); break;
-			case 3: delete(); break;
-			case 4: printAll(); break;
-			case 0:
+			case 1: insert(); break;	// 추가
+			case 2: update(); break;	// 전체 항목 수정
+			case 3: delete(); break;	// 삭제
+			case 4: printAll(); break;	// 전체 출력
+			case 5: update2(); break;	// 수정
+			case 0:	// 작업 끝
 				System.out.println("프로그램이 종료됩니다");
 				System.exit(0);
 
 			default:
-				System.out.println("잘못된 입력입니다");
+				System.out.println("번호를 잘못 입력했습니다. 다시 입력하세요");
 				break;
 			}
 			
@@ -73,31 +79,107 @@ public class JdbcTest06 {
 			
 		}
 	}
-
-	private void printAll() {
-		conn = DBUtill.getConnection();
-		String sql = "select * from mymember";
+	
+	// 회원정보를 수정하는 메서드 ==> 원하는 항목만 선택해서 수정하기
+	private void update2() {
+		System.out.println("자료를 수정할 아이디를 입력해주세요");
+		String id  = scan.next();
+		
+		int count = getMemberCount(id);
+		if(count == 0) {
+			System.out.println(id + "은(는) 없는 회원ID입니다.");
+			return;
+		}
+		
+		int num;
+		String updateField = null;
+		String updateTittle = null;
+		do {
+			System.out.println();
+			System.out.println("수정할 항목을 입력하세요");
+			System.out.println("1. 비밀번호  2. 회원이름  3. 전화번호  4. 회원주소");
+			System.out.println("------------------------------------");
+			System.out.print("수정항목 선택 >> ");
+			num = scan.nextInt();
+			
+			switch (num) {
+			case 1: updateField = "mem_pass";
+					updateTittle = "비밀번호"; break;
+			case 2: updateField = "mem_name";
+					updateTittle = "회원이름"; break;
+			case 3: updateField = "mem_tel";
+					updateTittle = "전화번호"; break;
+			case 4: updateField = "mem_addr";
+					updateTittle = "회원주소"; break;
+			default:
+				System.out.println("수정 항목을 잘못 선택했습니다");
+				System.out.println("다시 선택하세요");
+				break;
+			}
+		}while(num < 1 || num > 5);
+		
+		System.out.println();
+		System.out.print("새로운 " + updateTittle + " >> ");
+		scan.nextLine();	//버퍼 비우기
+		String updateData = scan.nextLine();
+		
 		try {
+			conn = DBUtill.getConnection();
+			String sql = "update mymember set " +
+					updateField + " = ? where mem_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, updateData);
+			pstmt.setString(2, id);
+			
+			int cnt = pstmt.executeUpdate();
+			
+			if(cnt > 0) {
+				System.out.println("수정 작업 성공");
+			}else {
+				System.out.println("수정 작업 실패");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disConnect();
+		}
+			
+		
+	}
+
+	// 전체 회원정보를 출력하는 메서드
+	private void printAll() {
+		System.out.println();
+		System.out.println("-----------------------------");
+		System.out.println(" ID  비밀번호  이름   전화번호  주소");
+		System.out.println("-----------------------------");
+		try {
+			conn = DBUtill.getConnection();
+			String sql = "select * from mymember";
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
-				System.out.println("아이디 | 이름 | 비밀번호 | 전화번호 | 주소 ");
-				System.out.println(rs.getString("mem_id") + " | " + rs.getString("mem_name") + " | " + rs.getString("mem_pass") + " | " 
+				System.out.println(rs.getString("mem_id") + " | " + rs.getString("mem_pass") + " | " + rs.getString("mem_name") + " | " 
 								 + rs.getString("mem_tel") + " | " + rs.getString("mem_addr"));
 			}
 		} catch (SQLException e) {
-		
+			e.printStackTrace();
+		}finally {
+			disConnect();
 		}
 	}
 
 	private void delete() {
-		conn = DBUtill.getConnection();
-		System.out.print("삭제할 아이디를 입력 > ");
+		System.out.println();
+		System.out.println("삭제할 회원정보를 입력하세요");
+		System.out.println("삭제할 회원ID >> ");
 		String id = scan.next();
 		
-		String sql = "delete from mymember where mem_id = ?";
 		try {
+			conn = DBUtill.getConnection();
+			String sql = "delete from mymember where mem_id = ?";
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			int result = pstmt.executeUpdate();
@@ -106,7 +188,12 @@ public class JdbcTest06 {
 			}else {
 				System.out.println("삭제 실패");
 			}
-		} catch (SQLException e) {}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disConnect();
+		}
 	}
 
 	private void update() {
@@ -115,26 +202,27 @@ public class JdbcTest06 {
 		String id  = scan.next();
 		
 		String sql = "select count(*) cnt from mymember where mem_id = ?";
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				if(rs.getInt("cnt") == 0) {
-					System.out.println("존재하지 않는 아이디입니다.");
-					return;
-				}
-			}
-		} catch (SQLException e) {}
 		
+		int count = getMemberCount(id);
+		if(count == 0) {
+			System.out.println(id + "은(는) 없는 회원ID입니다.");
+			return;
+		}
+		
+		System.out.println("수정할 내용을 입력하세요");
+	
 		System.out.print("이름 입력 >");
 		String name = scan.next();
+		
 		System.out.print("비밀번호 입력 >");
 		String pw = scan.next();
+		
 		System.out.print("전화번호 입력 >");
 		String tel = scan.next();
+
+		scan.nextLine();
 		System.out.print("주소 입력 >");
-		String addr = scan.next();
+		String addr = scan.nextLine();
 		
 		try {
 			sql = "update mymember set "
@@ -159,60 +247,93 @@ public class JdbcTest06 {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) try { rs.close(); } catch(SQLException e){}
-			if(stmt != null) try { stmt.close(); } catch(SQLException e){}
-			if(pstmt != null) try { pstmt.close(); } catch(SQLException e){}
-			if(conn != null) try {conn.close(); } catch(SQLException e){}
+			disConnect();
 		}
 		
 		
 		
 	}
 
-	private void insert() {
-		conn = DBUtill.getConnection();
-		String id = "";
-		while(true) {
-			System.out.print("아이디 입력 > ");
-			id = scan.next();
-			String sql = "select count(*) cnt from mymember where mem_id = ?";
-			
-			try {
-				pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, id);
-				rs = pstmt.executeQuery();
-				if(rs.next()) {
-					if(rs.getInt("cnt") == 0) break;
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-		
-			}
-			
-			
-		
-		}
-		
-		System.out.print("이름 입력 >");
-		String name = scan.next();
-		System.out.print("비밀번호 입력 >");
-		String pw = scan.next();
-		System.out.print("전화번호 입력 >");
-		String tel = scan.next();
-		System.out.print("주소 입력 >");
-		String addr = scan.next();
+	private int getMemberCount(String memId) {
+		int count = 0;
 		
 		try {
-			String sql = "insert into mymember values(?, ?, ?, ?, ?)";
+			conn = DBUtill.getConnection();
+			
+			String sql = "select count(*) cnt from mymember "
+					+ " where mem_id = ?";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, id);
-			pstmt.setString(2, name);
-			pstmt.setString(3, pw);
+			pstmt.setString(1, memId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("cnt");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			disConnect();
+		}
+		return count;
+	}
+	
+	// 사용했던 자원을 반납하는 메서드
+	private void disConnect() {
+		if(rs != null) try { rs.close(); } catch(SQLException e){}
+		if(stmt != null) try { stmt.close(); } catch(SQLException e){}
+		if(pstmt != null) try { pstmt.close(); } catch(SQLException e){}
+		if(conn != null) try {conn.close(); } catch(SQLException e){}
+	}
+	
+	private void insert() {
+		System.out.println();
+		System.out.println("추가할 회원 정보를 입력하세요");
+		
+		int count = 0;	// 입력한 회원ID의 개수가 저장될 변수
+		String memId = "";	// 회원ID가 저장될 변수
+		
+		do {
+			System.out.print("회원ID >> ");
+			memId = scan.next();
+			
+			// 회원ID를 매개변수로 받아서 해당 회원ID의 개수를 반환하는 메서드
+			count = getMemberCount(memId);
+			if(count>0) {
+				System.out.println(memId + "는 이미 등록된 ID입니다.");
+				System.out.println("다른 회원ID를 입력하세요");
+			}
+			
+		}while(count > 0);
+		
+		
+		System.out.print("회원이름 >>");
+		String name = scan.next();
+		
+		System.out.print("비밀번호 >>");
+		String pw = scan.next();
+		
+		System.out.print("전화번호 >>");
+		String tel = scan.next();
+		
+		scan.nextLine();  //입력버퍼 비우기
+		System.out.print("주소 >>");
+		String addr = scan.nextLine();
+		
+		try {
+			conn = DBUtill.getConnection();
+			String sql = "insert into mymember"
+					+ "(mem_id, mem_pass, mem_name, mem_tel, mem_addr)"
+					+ "values(?, ?, ?, ?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, memId);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, name);
 			pstmt.setString(4, tel);
 			pstmt.setString(5, addr);
 			
-			int result = pstmt.executeUpdate();
-			if(result > 0) {
+			int cnt = pstmt.executeUpdate();
+			if(cnt > 0) {
 				System.out.println("등록이 성공적으로 완료되었습니다");
 			}else {
 				System.out.println("등록실패");
@@ -220,10 +341,7 @@ public class JdbcTest06 {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) try { rs.close(); } catch(SQLException e){}
-			if(stmt != null) try { stmt.close(); } catch(SQLException e){}
-			if(pstmt != null) try { pstmt.close(); } catch(SQLException e){}
-			if(conn != null) try {conn.close(); } catch(SQLException e){}
+			disConnect();
 		}
 		
 	}
