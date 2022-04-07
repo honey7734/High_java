5e446660a87ccc3be53fd30669958acaa599573e
honey7734 <gusdml7243@gmail.com>
2022-04-07 15:02:06 +0900

    비동기 방식 연습(Non Ajax)

---
+++ testWeb/src/main/java/kr/or/ddit/basic/json/LprodController2.java
@@ -0,0 +1,46 @@
+package kr.or.ddit.basic.json;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+
+@WebServlet("/lprodController2.do")
+public class LprodController2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		LprodDao dao = LprodDao.getInstance();
+		
+		List<LprodVO> list = dao.getLprodList();
+		
+		// View로 보내줄 데이터를 request객체에 저장한다.
+		request.setAttribute("list", list);
+		
+		// View 페이지로 forwarding한다.
+		RequestDispatcher rd = request.getRequestDispatcher("/basic/json/lprodList.jsp");
+		
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- testWeb/src/main/webapp/basic/json/lprodMain.jsp
+++ testWeb/src/main/webapp/basic/json/lprodMain.jsp
@@ -13,16 +13,20 @@ $(function() {
 			url : '<%=request.getContextPath() %>/lprodController.do',
 			type : 'post',
 			success : function(data) {
-				var str = "";
+				var htmlCode = "<table border='1'>";
+				htmlCode += "<tr><td>LPROD_ID</td>"
+				htmlCode += "<td>LPROD_GU</td>"
+				htmlCode += "<td>LPROD_NM</td></tr>"
 				
 				$.each(data, function(i,v) {
-					str += i +"번째 자료<br>"
-					str += "id : " + v.lprod_id + "<br>"
-					str += "gu : " + v.lprod_gu + "<br>"
-					str += "nm : " + v.lprod_nm + "<hr>"
+					htmlCode += "<tr><td>" + v.lprod_id + "</td>" 
+					htmlCode += "<td>" + v.lprod_gu + "</td>" 
+					htmlCode += "<td>" + v.lprod_nm + "</td></tr>" 
 				})
 				
-				$("#result").html(str);
+				htmlCode += "</table>"
+				
+				$("#result").html(htmlCode);
 			},
 			error : function() {
 				alert('오류');
@@ -30,6 +34,11 @@ $(function() {
 			dataType : 'json'
 		})
 	})
+	
+	$('#lprodBtn2').on('click', function() {
+		location.href = "<%=request.getContextPath()%>/lprodController2.do";
+	})
+	
 })
 </script>
 </head>
@@ -38,7 +47,8 @@ $(function() {
 	DB의 LPROD테이블의 전체 데이터를 가져와 화면에 출력하시오
 -->
 <form>
-	<input type="button" id="lprodBtn" value="Lprod자료 가져오기">
+	<input type="button" id="lprodBtn" value="Lprod자료 가져오기(Ajax)">
+	<input type="button" id="lprodBtn2" value="Lprod자료 가져오기(Non Ajax)">
 </form>
 <h3>Lprod 자료 목록</h3>
 <div id="result"></div>
